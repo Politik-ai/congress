7dc7a84b2589e8087462b80d2414b274d217fe89
John Harrington <harrington.john@husky.neu.edu>
2020-05-27 13:10:53 -0400

    Changed directory info for most of building process

---
+++ get_texts.sh
@@ -0,0 +1,4 @@
+./run govinfo --collections=BILLS --congress=113 --store=pdf,mods,xml,text --extract=text,pdf,mods,xml
+./run govinfo --collections=BILLS --congress=114 --store=pdf,mods,xml,text --extract=text,pdf,mods,xml
+./run govinfo --collections=BILLS --congress=115 --store=pdf,mods,xml,text --extract=text,pdf,mods,xml
+./run govinfo --collections=BILLS --congress=116 --store=pdf,mods,xml,text --extract=text,pdf,mods,xml
