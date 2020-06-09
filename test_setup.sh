6f37373dacdf4cae30ed59917a569229d2151b5e
John Harrington <harrington.john@husky.neu.edu>
2020-06-08 21:51:26 -0400

    added vote line to test setup script

--- test_setup.sh
+++ test_setup.sh
@@ -1,3 +1,4 @@
 ./run govinfo --bulkdata=BILLSTATUS --congress=113
 ./run bills --congress=113
 ./run govinfo --collections=BILLS --congress=113 --store=pdf,mods,xml,text --extract=text,pdf,mods,xml
+./run votes --congress=113 --session=2014
